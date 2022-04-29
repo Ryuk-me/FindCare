295b31c24ccd1897aac25b232e97942066373ac0
ryuk-me <neerajishit@gmail.com>
2022-04-29 15:15:34 +0530

    🚀 deploy email verification

--- backend/app/Config.py
+++ backend/app/Config.py
@@ -30,12 +30,8 @@ class Settings(BaseSettings):
     MAIL_SERVER:str
     MAIL_FROM_NAME: str
 
-    if ENV == 'prod':
-        class Config:
-            env_file = ".env"
-    else:
-        class Config:
-            env_file = ".env.local"
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
@@ -43,9 +39,3 @@ settings = Settings()
 if ENV == 'prod':
     settings.DATABASE_HOSTNAME = "postgres"
     settings.API_HOSTED_ROOT_URL = "https://nextcare-api-ryuk-me.cloud.okteto.net/"
-    settings.MAIL_USERNAME = os.environ['MAIL_USERNAME']
-    settings.MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
-    settings.MAIL_FROM = os.environ['MAIL_FROM']
-    settings.MAIL_PORT = os.environ['MAIL_PORT']
-    settings.MAIL_SERVER = os.environ['MAIL_SERVER']
-    settings.MAIL_FROM_NAME = os.environ['MAIL_FROM_NAME']
