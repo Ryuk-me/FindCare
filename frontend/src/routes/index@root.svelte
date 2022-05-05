ac5eeb6c4633f57d8035481f3963c2e10631494d
ryuk-me <neerajishit@gmail.com>
2022-05-06 04:28:56 +0530

    ✨ pre-rendered reset route

--- frontend/src/routes/index@root.svelte
+++ frontend/src/routes/index@root.svelte
@@ -1,19 +1,23 @@
+<script context="module">
+	export const prerender = true
+</script>
+
 <svelte:head>
-  <title>FindCare</title>
+	<title>Reset Password</title>
 </svelte:head>
 
 <div class="container-home">
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<button><a href='/login'>login</a></button>
+	<button><a href="/login">login</a></button>
 </div>
 
 <style>
-    .container-home{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-    }
+	.container-home {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
 </style>
