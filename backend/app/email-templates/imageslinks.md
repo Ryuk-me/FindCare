7ae3292ce4ee3b247f9daaac1195d4f85fe7711c
ryuk-me <neerajishit@gmail.com>
2022-05-06 20:13:53 +0530

    🚧 cleaned email-templates folder

--- backend/app/email-templates/imageslinks.md
+++
@@ -1,6 +0,0 @@
-#
-
-> https://i.ibb.co/MZ7XPf0/hospital.png
-> https://i.ibb.co/34y98dQ/Rectangle-left.png
-> https://i.ibb.co/1sbrddB/Rectangle-right.png
-> https://i.ibb.co/KwKqp2J/verify.png
--- backend/app/services.py
+++ backend/app/services.py
@@ -498,7 +498,6 @@ def calculate_slots(opens_at, closes_at, session_time):
 
 async def send_email(subject: str, recipients: str, token: str, token_url: str):
     message = MessageSchema(
-        subtype='html',
         subject=subject,
         recipients=[recipients],
         template_body={
@@ -508,7 +507,7 @@ async def send_email(subject: str, recipients: str, token: str, token_url: str):
 
     conf = login_mail()
     fm = FastMail(conf)
-    await fm.send_message(message, template_name='verify.html')
+    await fm.send_message(message, template_name='new-user.html')
 
     #! CHANGE THIS TO EMAIL SENT SUCCESSFULLY PLEASE VERIFY
     return {"details": "email sent successfully"}
