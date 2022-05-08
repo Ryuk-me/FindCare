125bcbde2a23561f0cb25cd84f63a1f2d4fa6d66
ryuk-me <neerajishit@gmail.com>
2022-05-08 17:33:13 +0530

    ♻️ capitalize all backend error messages

--- backend/app/error_handlers/errors.py
+++ backend/app/error_handlers/errors.py
@@ -9,22 +9,22 @@ from fastapi import HTTPException, status
 
 
 USER_NOT_FOUND = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail=f'user not found')
+    status_code=status.HTTP_404_NOT_FOUND, detail=f'User not found')
 
 USER_ALREADY_EXIST = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="user already exist")
+    status_code=status.HTTP_409_CONFLICT, detail="User already exist")
 
 USER_ALREADY_BANNED = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="user is already banned")
+    status_code=status.HTTP_409_CONFLICT, detail="User is already banned")
 
 USER_ALREADY_UNBANNED = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="user is not banned")
+    status_code=status.HTTP_409_CONFLICT, detail="User is not banned")
 
 USER_IS_BANNED = HTTPException(
     status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Your account has been blocked. Please contact support for further details")
 
 NO_USER_FOUND = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail=f'no user found')
+    status_code=status.HTTP_404_NOT_FOUND, detail=f'No user found')
 
 # ***********************************************************************************
 #                                                                                   #
@@ -34,34 +34,34 @@ NO_USER_FOUND = HTTPException(
 
 
 DOCTOR_ALREADY_EXIST = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="doctor already exist")
+    status_code=status.HTTP_409_CONFLICT, detail="Doctor already exist")
 
 DOCTOR_NOT_FOUND = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail=f'doctor not found')
+    status_code=status.HTTP_404_NOT_FOUND, detail=f'Doctor not found')
 
 NO_DOCTOR_FOUND_WITH_THIS_ID = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail="no doctor found with this id")
+    status_code=status.HTTP_404_NOT_FOUND, detail="No doctor found with this id")
 
 DOCTOR_IS_ALREADY_VERIFIED = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="doctor is already verified")
+    status_code=status.HTTP_409_CONFLICT, detail="Doctor is already verified")
 
 DOCTOR_WITH_THIS_REGISTRATION_NUM_ALREADY_EXIST = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="doctor with this registration number already exist")
+    status_code=status.HTTP_409_CONFLICT, detail="Doctor with this registration number already exist")
 
 NOT_POSSIBLE_EXPERINCE_YEAR = HTTPException(
-    status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="experince year not acceptable please try lower value")
+    status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Experince year not acceptable please try lower value")
 
 DOCTOR_IS_NOT_VERIFIED = HTTPException(
-    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="doctor is not verified")
+    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Doctor is not verified")
 
 DOCTOR_IS_BANNED = HTTPException(
-    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="doctor is banned")
+    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Doctor is banned")
 
 DOCTOR_IS_ALREADY_BANNED = HTTPException(
-    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="doctor is already banned")
+    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Doctor is already banned")
 
 DOCTOR_IS_ALREADY_UNBANNED = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="doctor is not banned")
+    status_code=status.HTTP_409_CONFLICT, detail="Doctor is not banned")
 
 # ***********************************************************************************
 #                                                                                   #
@@ -78,20 +78,20 @@ DOCTOR_IS_ALREADY_UNBANNED = HTTPException(
 
 
 INVALID_CREDENTIALS_ERROR = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect username or password.")
+    status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect Email or Password.")
 
 
 TOKEN_CREDENTIALS_ERROR = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN, detail="could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
 
 FORBIDDEN_ACTION_ERROR = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN, detail="not authorised to perform this action")
+    status_code=status.HTTP_403_FORBIDDEN, detail="Not authorised to perform this action")
 
 PLEASE_LOGIN_FIRST = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN, detail=f'please login first')
+    status_code=status.HTTP_403_FORBIDDEN, detail=f'Please login first')
 
 NOT_A_SUPER_ADMIN = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN, detail="you dont have enough permission to perform this action")
+    status_code=status.HTTP_403_FORBIDDEN, detail="You dont have enough permission to perform this action")
 
 
 # ***********************************************************************************
@@ -102,13 +102,13 @@ NOT_A_SUPER_ADMIN = HTTPException(
 
 
 ALREADY_EXIST_CLINIC = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="clinic already exist for this doctor")
+    status_code=status.HTTP_409_CONFLICT, detail="Clinic already exist for this doctor")
 
 CLINIC_NOT_FOUND = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail="clinic not found")
+    status_code=status.HTTP_404_NOT_FOUND, detail="Clinic not found")
 
 CLINIC_IS_NOT_SERVICEABLE = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail="clinic is not servicable currently")
+    status_code=status.HTTP_404_NOT_FOUND, detail="Clinic is not servicable currently")
 
 
 # ***********************************************************************************
@@ -119,30 +119,30 @@ CLINIC_IS_NOT_SERVICEABLE = HTTPException(
 
 
 APPOINTNEMT_ALREADY_CANCELLED = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="appointment is already cancelled")
+    status_code=status.HTTP_409_CONFLICT, detail="Appointment is already cancelled")
 
 APPOINTMENT_ALREADY_SKIPPED = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="appointment is already skipped")
+    status_code=status.HTTP_409_CONFLICT, detail="Appointment is already skipped")
 
 APPOINTNEMT_ALREADY_CANCELLED_BY_USER = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="appointment is already cancelled by the user")
+    status_code=status.HTTP_409_CONFLICT, detail="Appointment is already cancelled by the user")
 
 APPOINTMENT_ALREADY_CANCELLED_BY_DR = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="appointment is already cancelled by the doctor")
+    status_code=status.HTTP_409_CONFLICT, detail="Appointment is already cancelled by the doctor")
 
 APPOINTMENT_ALREADY_COMPLETED = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="appointment is already completed")
+    status_code=status.HTTP_409_CONFLICT, detail="Appointment is already completed")
 
 APPOINTMENT_SKIPPED_CANCELLATION = HTTPException(
     status_code=status.HTTP_409_CONFLICT,
-    detail="appointment cancelled due to patient was not on time"
+    detail="Appointment cancelled due to patient was not on time"
 )
 
 NO_APPOINTMENT_FOUND_ERROR = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail="no appointment found")
+    status_code=status.HTTP_404_NOT_FOUND, detail="No appointment found")
 
 NO_CANCELLATION_REASON = HTTPException(
-    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="please enter a cancellation reason")
+    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Please enter a cancellation reason")
 
 
 # ***********************************************************************************
@@ -152,7 +152,7 @@ NO_CANCELLATION_REASON = HTTPException(
 # ***********************************************************************************
 
 NOTHING_CHANGED = HTTPException(
-    status_code=status.HTTP_304_NOT_MODIFIED, detail="not modified")
+    status_code=status.HTTP_304_NOT_MODIFIED, detail="Not modified")
 
 # ***********************************************************************************
 #                                                                                   #
@@ -162,20 +162,20 @@ NOTHING_CHANGED = HTTPException(
 
 
 NOT_FOUND_ERROR = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail="not found")
+    status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
 
 
 PHONE_NUMBER_ALREADY_EXIST = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="an account with this phone number already exist")
+    status_code=status.HTTP_409_CONFLICT, detail="An account with this phone number already exist")
 
 EMAIL_ALREADY_EXIST = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="an account with this email already exist")
+    status_code=status.HTTP_409_CONFLICT, detail="An account with this email already exist")
 
 PASSWORD_CANNOT_BE_SAME = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="password cannot be same as the old one")
+    status_code=status.HTTP_409_CONFLICT, detail="Password cannot be same as the old one")
 
 ACCOUNT_NOT_FOUND_WITH_THIS_EMAIL = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail="no account found with this email")
+    status_code=status.HTTP_404_NOT_FOUND, detail="No account found with this email")
 
 
 # ***********************************************************************************
@@ -186,10 +186,10 @@ ACCOUNT_NOT_FOUND_WITH_THIS_EMAIL = HTTPException(
 
 
 VERIFICATION_LINK_EXPIRED = HTTPException(
-    status_code=status.HTTP_410_GONE, detail="verification link has been expired")
+    status_code=status.HTTP_410_GONE, detail="Verification link has been expired")
 
 EMAIL_ALREADY_VERIFIED = HTTPException(
-    status_code=status.HTTP_409_CONFLICT, detail="email already verified")
+    status_code=status.HTTP_409_CONFLICT, detail="Email already verified")
 
 PLEASE_VERIFY_YOUR_EMAIL = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN, detail="please verify your email first")
+    status_code=status.HTTP_403_FORBIDDEN, detail="Please verify your Email first")
