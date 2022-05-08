ed8ab5c17e0e036d722cedfa31a707ca909c18ca
ADGAMERX <amank.u.m100@gmail.com>
2022-05-09 02:31:17 +0530

    ⚡️ improved ui of signup page

--- frontend/src/routes/register-doctor/index@root.svelte
+++ frontend/src/routes/register-doctor/index@root.svelte
@@ -2,28 +2,30 @@
 	import doctor_png from '$lib/assets/doctor/doctor.png'
 	import lower_png from '$lib/assets/doctor/lower.png'
 	import upper_png from '$lib/assets/doctor/upper.png'
+	let show = false
 </script>
 
 <svelte:head>
 	<title>Findcare Register As Doctor</title>
 </svelte:head>
-<div class="w-full h-screen">
+<div class="w-full h-full">
 	<img src={doctor_png} alt="doctor.png" class="fixed hidden lg:block bottom-0 ml-7 left-0 w-40" />
-	<img src={upper_png} alt="upper.png" class="fixed hidden lg:block right-0 top-0" />
+	<img src={upper_png} alt="upper.png" class="fixed lg:block right-0 top-0" />
 	<img src={lower_png} alt="lower.png" class="fixed hidden lg:block right-0 bottom-0" />
-	<div class="flex justify-center items-center h-full">
+	<img src={lower_png} alt="lower.png" class="fixed block lg:hidden left-0 bottom-0 scale-x-[-1]" />
+	<div class="flex justify-center items-center">
 		<div class="my-2">
-			<div class="text-primary font-bold font-poppins text-4xl">Findcare</div>
-			<div class="flex justify-center items-center">
+			<div class="text-primary font-bold font-poppins text-4xl mb-2 lg:pl-7">FindCare</div>
+			<div class="flex justify-center items-center font-medium font-maven">
 				<form
 					action=""
-					class="flex flex-col justify-center items-start w-[90vw] lg:w-[70vw] md:w-[70vw] bg-white rounded drop-shadow-xl mb-8 px-4 py-7"
+					class="flex flex-col justify-center items-start w-[90vw] lg:w-[75vw] md:w-[70vw] bg-white lg:bg-transparent  rounded drop-shadow-xl mb-8 px-7 pb-7 pt-4 lg:border-none border border-primary"
 				>
 					<div class=" w-full">
-						<div class="font-bold my-3 text-2xl">
-							Register as <span class="text-primary">Doctor</span>
+						<div class="font-bold mb-3 text-2xl">
+							Register as <span class="text-primary font-extrabold">Doctor</span>
 						</div>
-						<div class="text-sm">
+						<div class="text-sm font-normal">
 							Let's get you all set up so you can verify your personal account and begin setting up
 							your profile
 						</div>
@@ -39,7 +41,8 @@
 								type="text"
 								name="firstName"
 								id="firstName"
-								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								placeholder="Dr. Neeraj"
+								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 						<div class="relative w-full mb-4 lg:mx-4">
@@ -47,17 +50,19 @@
 							<input
 								type="text"
 								name="lastName"
+								placeholder="Kumar"
 								id="lastName"
-								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 						<div class="relative w-full mb-4">
 							<label for="registrationNumber">Registration Number</label>
 							<input
 								type="text"
+								placeholder="AQ-15-XXXXX"
 								name="registrationNumber"
 								id="registrationNumber"
-								class="block appearance-none border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								class="block appearance-none border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 					</div>
@@ -66,18 +71,20 @@
 							<label for="phoneNumber">Phone Number</label>
 							<input
 								type="number"
+								placeholder="98765XXXXX"
 								name="phoneNumber"
 								id="phoneNumber"
-								class="block appearance-none border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								class="block appearance-none border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 						<div class="relative w-full mb-4 lg:mx-4">
 							<label for="email">Email</label>
 							<input
 								type="email"
+								placeholder="your@domain.com"
 								name="email"
 								id="email"
-								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 						<div class="relative w-full mb-4">
@@ -86,7 +93,7 @@
 								type="text"
 								name="speciality"
 								id="speciality"
-								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 					</div>
@@ -97,24 +104,32 @@
 								type="date"
 								name="dob"
 								id="dob"
-								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 						<div class="relative w-full mb-4 lg:mx-4">
 							<label for="gender">Gender</label>
 							<div class="flex items-center pt-2 lg:pt-5">
-								<div class="flex items-center mr-5">
-									<input id="male" type="radio" name="gender" class="mr-2" />
+								<div class="flex items-center ">
+									<input id="male" type="radio" name="gender" class="mr-2" checked />
 									<label class="form-check-label inline-block text-gray-800" for="male" value="male"
 										>Male</label
 									>
 								</div>
-								<div class="flex items-center">
+								<div class="flex items-center mx-5">
 									<input id="female" type="radio" name="gender" class="mr-2" />
 									<label
 										class="form-check-label inline-block text-gray-800"
 										for="female"
 										value="female">Female</label
+									>
+								</div>
+								<div class="flex items-center">
+									<input id="other" type="radio" name="gender" class="mr-2" />
+									<label
+										class="form-check-label inline-block text-gray-800"
+										for="other"
+										value="other">Other</label
 									>
 								</div>
 							</div>
@@ -125,27 +140,44 @@
 								type="number"
 								name="expYear"
 								id="expYear"
-								class="block appearance-none border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								min="1"
+								max="70"
+								placeholder="20"
+								class="block appearance-none border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 					</div>
 					<div class="w-full lg:flex lg:justify-center">
 						<div class="relative w-full mb-4">
 							<label for="password">Password</label>
-							<input
-								type="password"
-								name="password"
-								id="password"
-								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
-							/>
+							<div class="relative">
+								<input
+									type={show ? 'text' : 'password'}
+									name="password"
+									placeholder="***********"
+									id="password"
+									class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
+								/>
+								<span>
+									<i
+										class="fa {show
+											? 'fa-eye-slash'
+											: 'fa-eye'} hover:cursor-pointer text-slate-600"
+										aria-hidden="true"
+										id="eye"
+										on:click|preventDefault={() => (show = !show)}
+									/>
+								</span>
+							</div>
 						</div>
 						<div class="relative w-full mb-4 lg:mx-4">
 							<label for="confirmPassword">Confirm Password</label>
 							<input
 								type="password"
 								name="confirmPassword"
+								placeholder="***********"
 								id="confirmPassword"
-								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 						<div class="relative w-full mb-4">
@@ -153,20 +185,21 @@
 							<input
 								type="text"
 								name="about"
+								placeholder="Tell us about yourself"
 								id="about"
-								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary"
+								class="block border rounded py-2 px-3 w-full mt-3 focus:outline-none focus:shadow-outline focus:ring-1 focus:ring-primary" required
 							/>
 						</div>
 					</div>
 					<div class="w-full">
 						<div class="w-full">
 							<button
-								class="bg-primary hover:bg-[#524af4] lg:w-[16vw] text-white mb-3 font-bold py-2 w-full rounded focus:outline-none focus:shadow-outline"
+								class="bg-primary hover:bg-[#524af4] lg:w-[16vw] text-white my-3 font-bold py-2 w-full rounded focus:outline-none focus:shadow-outline"
 								>create account</button
 							>
 						</div>
 						<div>
-							Already have an account? <a href="./login" class="text-primary">Login</a>
+							Already have an account? <a href="./login" class="text-primary hover:font-semibold">Login</a>
 						</div>
 					</div>
 				</form>
@@ -188,4 +221,11 @@
 		-o-user-drag: none;
 		/* user-drag: none; */
 	}
+	#eye {
+		position: absolute;
+		right: 14px;
+		transform: translate(0, -50%);
+		top: 50%;
+	}
+
 </style>
