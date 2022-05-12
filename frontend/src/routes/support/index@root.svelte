0fc4b13d2235d2b35113f51eb8b5c78e6109fb8e
ADGAMERX <amank.u.m100@gmail.com>
2022-05-12 19:47:56 +0530

    ✨ Added Live Chat Support Page

---
+++ frontend/src/routes/support/index@root.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte'
+	import Navbar from '$lib/components/Navbar.svelte'
+</script>
+
+<div class="h-screen w-screen">
+	<Navbar />
+
+	<div
+		class="w-full justify-center items-center flex-col rounded-lg h-[75vh] overflow-hidden sm:mr-10 p-10 flex  relative"
+	>
+		<iframe
+			width="100%"
+			height="100%"
+			class="absolute inset-0"
+			frameborder="0"
+			title="live-chat"
+			marginheight="0"
+			marginwidth="0"
+			scrolling="no"
+			loading="lazy"
+			referrerpolicy="no-referrer-when-downgrade"
+			src="https://tawk.to/findcare"
+		/>
+	</div>
+
+	<Footer />
+</div>
