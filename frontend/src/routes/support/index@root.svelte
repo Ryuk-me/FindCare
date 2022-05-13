5b8d7713100c040728dfe5be4c121aeba5464fbe
ADGAMERX <amank.u.m100@gmail.com>
2022-05-13 19:27:07 +0530

    ➕ Added Support Page Heading

--- frontend/src/routes/support/index@root.svelte
+++ frontend/src/routes/support/index@root.svelte
@@ -5,10 +5,11 @@
 
 <div class="h-screen w-screen">
 	<Navbar />
-
+	<h1 class="text-center text-6xl my-6 text-gray-600 font-bold">Live Chat</h1>
 	<div
 		class="w-full justify-center items-center flex-col rounded-lg h-[75vh] overflow-hidden sm:mr-10 p-10 flex  relative"
 	>
+
 		<iframe
 			width="100%"
 			height="100%"
