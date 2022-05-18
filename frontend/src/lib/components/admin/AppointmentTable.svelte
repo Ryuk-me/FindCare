8f229d6df30b2deb4a0e6286fe3f1905640d2859
ADGAMERX <amank.u.m100@gmail.com>
2022-05-18 16:42:59 +0530

    ✨ Appointment Page Done

---
+++ frontend/src/lib/components/admin/AppointmentTable.svelte
@@ -0,0 +1,71 @@
+<script>
+	// core components
+	import UserTableStats from '$lib/components/admin/UserTableStats.svelte';
+import AppointmentTableStats from './AppointmentTableStats.svelte';
+	// can be one of light or dark
+	export let color = 'light'
+</script>
+
+<div
+	class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded {color === 'light'
+		? 'bg-white'
+		: 'bg-red-800 text-white'}"
+>
+	<div class="rounded-t mb-0 px-4 py-3 border-0">
+		<div class="flex flex-wrap items-center">
+			<div class="relative w-full px-4 max-w-full flex-grow flex-1">
+				<h3 class="font-semibold text-lg {color === 'light' ? 'text-blueGray-700' : 'text-white'}">
+					Appointments Details
+				</h3>
+			</div>
+		</div>
+	</div>
+	<div class="block w-full overflow-x-auto">
+		<!-- Projects table -->
+		<table class="items-center w-full bg-transparent border-collapse">
+			<thead>
+				<tr>
+					<th
+						class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color ===
+						'light'
+							? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+							: 'bg-red-700 text-red-200 border-red-600'}"
+					>
+						Doctor's Name
+					</th>
+					<th
+						class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color ===
+						'light'
+							? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+							: 'bg-red-700 text-red-200 border-red-600'}"
+					>
+						Date Of Appointment
+					</th>
+					<th
+						class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color ===
+						'light'
+							? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+							: 'bg-red-700 text-red-200 border-red-600'}"
+					>
+						Status
+					</th>
+					
+					<th
+						class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color ===
+						'light'
+							? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+							: 'bg-red-700 text-red-200 border-red-600'}"
+					>
+						Clinic Address
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<AppointmentTableStats />
+				<AppointmentTableStats doctorName="Nitin Shivam" dateOfAppointment="22-05-2022" status="cancelled" clinicAddress="Boring Road, Patna" />
+				<AppointmentTableStats doctorName="Amardeep" dateOfAppointment="12-04-2022" status="completed" clinicAddress="Gandhi Maidan, Patna" />
+				
+			</tbody>
+		</table>
+	</div>
+</div>
---
+++ frontend/src/lib/components/admin/AppointmentTableStats.svelte
@@ -0,0 +1,56 @@
+<script>
+    import TableDropdown from "$lib/components/TableDropdown.svelte";
+    export let doctorImg = 'https://cdn-icons-png.flaticon.com/512/4825/4825038.png'
+    export let doctorName = 'Neeraj Kumar'
+    export let clinicAddress = 'Punaichak, Patna'
+    export let dateOfAppointment = '25-05-2022'
+    export let status = 'upcoming'
+    export let color = "light";
+</script>
+
+<tr>
+   <th
+     class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center"
+   >
+     <img
+       src="{doctorImg}"
+       class="h-12 w-12 bg-white rounded-full border"
+       alt="..."
+     />
+     <span
+       class="ml-3 font-bold {color === 'light' ? 'btext-blueGray-600' : 'text-whit'}"
+     >
+       {doctorName}
+     </span>
+   </th>
+   <td
+     class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+   >
+     {dateOfAppointment}
+   </td>
+   <td
+     class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+   >
+   {#if status == 'completed' }
+   <i class="fas fa-circle text-teal-500 mr-2"></i> {status}
+   
+   {/if}
+   {#if status == 'cancelled' }
+   <i class="fas fa-circle text-red-500 mr-2"></i> {status}
+       
+   {/if}
+   {#if status == 'upcoming' }
+   <i class="fas fa-circle text-orange-500 mr-2"></i> {status}
+       
+   {/if}
+   </td>
+
+   <td
+     class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+   >
+    {clinicAddress}
+      
+  
+   </td>
+ 
+ </tr>
