80924a7d5ec4c7adad1a4ba1ee1cc3377c68faba
Nitin Shivam <nitinshivam2000@gmail.com>
2022-05-23 00:17:43 +0530

    🎨 updated about page

--- frontend/src/routes/about-us/index@root.svelte
+++ frontend/src/routes/about-us/index@root.svelte
@@ -95,6 +95,42 @@
 									<img
 										alt="team"
 										class="flex-shrink-0 rounded-full w-full h-56 object-cover object-center mb-4"
+										src="https://avatars.githubusercontent.com/u/61681057?v=4"
+									/>
+									<div class="w-full">
+										<h2 class="title-font font-semibold text-lg text-gray-900">Nitin Shivam</h2>
+										<h3 class="text-gray-500 mb-3">Frontend Developer</h3>
+										<p class="mb-4">To develop website features that can enhance the user experience.</p>
+										<span class="inline-flex">
+											<a
+												href="https://github.com/nitinshivam"
+												target="_blank"
+												class="text-gray-500"
+											>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													width="24"
+													height="24"
+													shape-rendering="geometricPrecision"
+													text-rendering="geometricPrecision"
+													image-rendering="optimizeQuality"
+													fill-rule="evenodd"
+													clip-rule="evenodd"
+													viewBox="0 0 640 640"
+													><path
+														d="M319.988 7.973C143.293 7.973 0 151.242 0 327.96c0 141.392 91.678 261.298 218.826 303.63 16.004 2.964 21.886-6.957 21.886-15.414 0-7.63-.319-32.835-.449-59.552-89.032 19.359-107.8-37.772-107.8-37.772-14.552-36.993-35.529-46.831-35.529-46.831-29.032-19.879 2.209-19.442 2.209-19.442 32.126 2.245 49.04 32.954 49.04 32.954 28.56 48.922 74.883 34.76 93.131 26.598 2.882-20.681 11.15-34.807 20.315-42.803-71.08-8.067-145.797-35.516-145.797-158.14 0-34.926 12.52-63.485 32.965-85.88-3.33-8.078-14.291-40.606 3.083-84.674 0 0 26.87-8.61 88.029 32.8 25.512-7.075 52.878-10.642 80.056-10.76 27.2.118 54.614 3.673 80.162 10.76 61.076-41.386 87.922-32.8 87.922-32.8 17.398 44.08 6.485 76.631 3.154 84.675 20.516 22.394 32.93 50.953 32.93 85.879 0 122.907-74.883 149.93-146.117 157.856 11.481 9.921 21.733 29.398 21.733 59.233 0 42.792-.366 77.28-.366 87.804 0 8.516 5.764 18.473 21.992 15.354 127.076-42.354 218.637-162.274 218.637-303.582 0-176.695-143.269-319.988-320-319.988l-.023.107z"
+													/></svg
+												>
+											</a>
+										</span>
+									</div>
+								</div>
+							</div>
+							<div class="p-4 lg:w-1/4 md:w-1/2">
+								<div class="h-full flex flex-col items-center text-center">
+									<img
+										alt="team"
+										class="flex-shrink-0 rounded-full w-full h-56 object-cover object-center mb-4"
 										src="https://findcare-ryuk-me.cloud.okteto.net/dev/amardeep.jpg"
 									/>
 									<div class="w-full">
@@ -156,42 +192,7 @@
 									</div>
 								</div>
 							</div>
-							<div class="p-4 lg:w-1/4 md:w-1/2">
-								<div class="h-full flex flex-col items-center text-center">
-									<img
-										alt="team"
-										class="flex-shrink-0 rounded-full w-full h-56 object-cover object-center mb-4"
-										src="https://avatars.githubusercontent.com/u/61681057?v=4"
-									/>
-									<div class="w-full">
-										<h2 class="title-font font-semibold text-lg text-gray-900">Nitin Shivam</h2>
-										<h3 class="text-gray-500 mb-3">Database Administrator</h3>
-										<p class="mb-4">Creating database design, creation, and testing activities.</p>
-										<span class="inline-flex">
-											<a
-												href="https://github.com/nitinshivam"
-												target="_blank"
-												class="text-gray-500"
-											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													width="24"
-													height="24"
-													shape-rendering="geometricPrecision"
-													text-rendering="geometricPrecision"
-													image-rendering="optimizeQuality"
-													fill-rule="evenodd"
-													clip-rule="evenodd"
-													viewBox="0 0 640 640"
-													><path
-														d="M319.988 7.973C143.293 7.973 0 151.242 0 327.96c0 141.392 91.678 261.298 218.826 303.63 16.004 2.964 21.886-6.957 21.886-15.414 0-7.63-.319-32.835-.449-59.552-89.032 19.359-107.8-37.772-107.8-37.772-14.552-36.993-35.529-46.831-35.529-46.831-29.032-19.879 2.209-19.442 2.209-19.442 32.126 2.245 49.04 32.954 49.04 32.954 28.56 48.922 74.883 34.76 93.131 26.598 2.882-20.681 11.15-34.807 20.315-42.803-71.08-8.067-145.797-35.516-145.797-158.14 0-34.926 12.52-63.485 32.965-85.88-3.33-8.078-14.291-40.606 3.083-84.674 0 0 26.87-8.61 88.029 32.8 25.512-7.075 52.878-10.642 80.056-10.76 27.2.118 54.614 3.673 80.162 10.76 61.076-41.386 87.922-32.8 87.922-32.8 17.398 44.08 6.485 76.631 3.154 84.675 20.516 22.394 32.93 50.953 32.93 85.879 0 122.907-74.883 149.93-146.117 157.856 11.481 9.921 21.733 29.398 21.733 59.233 0 42.792-.366 77.28-.366 87.804 0 8.516 5.764 18.473 21.992 15.354 127.076-42.354 218.637-162.274 218.637-303.582 0-176.695-143.269-319.988-320-319.988l-.023.107z"
-													/></svg
-												>
-											</a>
-										</span>
-									</div>
-								</div>
-							</div>
+							
 						</div>
 					</div>
 				</section>
